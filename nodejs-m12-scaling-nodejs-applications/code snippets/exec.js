53860faed7d6ad47ee1e41495882fd89922386d4
baluragala <baluragala@gmail.com>
2017-07-27 12:19:28 +0530

    added m14 mongo slides and queries

--- nodejs-m12-scaling-nodejs-applications/code snippets/exec.js
+++
@@ -1,8 +0,0 @@
-const exec = require('child_process').exec;
-const child = exec('ps -ef | grep node', (error, stdout, stderr) => {
-  if (error) {
-    console.error('stderr', stderr);
-    throw error;
-  }
-  console.log('stdout', stdout);
-});
